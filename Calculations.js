ef2a238806561b369c4a7ef39d7a4ca61e874aa6
Rasmus <rasmustofthermansen@gmail.com>
2023-10-03 19:29:16 +0200

    Keyboard event

--- Calculations.js
+++ Calculations.js
@@ -1,19 +1,27 @@
 const display = document.getElementById('display');
 
-        function clearDisplay() {
-            display.value = '';
-        }
+function clearDisplay() {
+    display.value = '';
+}
 
-        function deleteLast() {
-            display.value = display.value.slice(0, -1);
-        }
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
 
-        function appendChar(char) {
-            display.value += char;
-        }
+function appendChar(char) {
+    display.value += char;
+}
 
-        function calculate() {
-            let result = eval(display.value);
-            result = Math.round(result * 100) / 100; 
-            display.value = result;
-        }
+function calculate() {
+    let result = eval(display.value);
+    result = Math.round(result * 100) / 100;
+    display.value = result;
+}
+
+// Add event listener for Enter key
+display.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();  
+        calculate();
+    }
+});
